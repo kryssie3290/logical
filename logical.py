d997b8e9b1c9a01408512d13b9d5a177450dfb3d
kryssie3290 <christabeladamu2@gmail.com>
2025-07-16 05:16:10 -0700

    logical.py

---
+++ logical.py
@@ -0,0 +1,53 @@
+'''
+first_number = 4
+ second_number = 5
+# Arithmetic operators
+# plus +
+sub = first_number - second_number
+print(sub) 15 first_name = "tom"
+ '''
+print("welcome to our simple calculator app")
+print('''1. addition
+2. subtraction
+3. multiplication
+4. division
+5. expo
+'''
+)
+print("enter two numbers to add")
+first_number = float(input("first_number:"))
+second_number = float(input("second_number:"))
+sum = first_number + second_number
+print(f"{first_number} + {second_number} = {sum:.2f}")
+print("**************************")
+
+print("enter two numbers to sub")
+first_number = float(input("first_number:"))
+second_number = float(input("second_number:"))
+sum = first_number - second_number
+print(f"{first_number} - {second_number} = {sum:.2f}")
+print("**********************")
+ 
+print("enter two numbers to mul")
+first_number = float(input("first_number:"))
+second_number = float(input("second_number:"))
+sum = first_number * second_number
+print(f"{first_number} * {second_number} = {sum:.2f}")
+print("***********************")
+print("enter two numbers to divide")
+first_number = float(input("first_number:"))
+second_number = float(input("second_number:"))
+sum = first_number / second_number
+print(f"{first_number} divide by {second_number} = {sum:.2f}")
+print("**********************")
+print("enter two numbers to expo")
+first_number = float(input("first_number:"))
+second_number = float(input("second_number:"))
+sum = first_number ** second_number
+print(f"{first_number} ** {second_number} = {sum:.2f}")
+
+
+
+
+
+
