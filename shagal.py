3d6af8691ed96fde6fa33841bfc1315779bedd02
kryssie3290 <christabeladamu2@gmail.com>
2025-07-17 20:11:58 -0700

    shagal.py

---
+++ shagal.py
@@ -0,0 +1,17 @@
+
+allowance = 2000
+allowance -= 400
+print(f"remaining allowance:{allowance}")
+allowance = allowance + 100
+print(allowance)
+allowance -=250
+print(f"allowance: {allowance}")
+print("discount: 25%")
+percent = 25 / 100
+percent *= allowance
+allowance -= percent
+print(f"allowance: {allowance}")
+allowance = 1/3
+print(f"allowance: {allowance}")
+allowance = 100 % 0.16666665 
+print(f"allowance: {allowance}")
